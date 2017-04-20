9aae8eefa12e3f801d588094b576da87a3818ffc
JoshuaBeard <joshuabeard92@gmail.com>
2017-04-20 15:54:22 -0700

    vim install

--- ubsetup.sh
+++ ubsetup.sh
@@ -1,8 +1,10 @@
 ## script for setting up useful preferences in ubuntu on my personal machine
 
 # cs <dir>: combines cd <dir> and ls for simple navigation
-echo '# combines cd and ls in one easy command' >> ~/.bashrc
+echo '# CS: combines cd and ls in one easy command' >> ~/.bashrc
 echo 'function cs () {' >> ~/.bashrc
 echo '  cd "$@" && ls' >> ~/.bashrc
 echo '}' >> ~/.bashrc
 
+# Get vim
+sudo apt-get install vim
