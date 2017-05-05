362d9f2c7f6248a9d429d665b5466ff115c9b69a
JoshuaBeard <beardj1@seattleu.edu>
2017-05-04 18:05:22 -0700

    automake and autoconf

--- ubsetup.sh
+++ ubsetup.sh
@@ -14,7 +14,11 @@ echo '}' >> ~/.bashrc
 
 # Get vim
 sudo apt-get install -y vim
-
+# Set vim indent to four spaces
 echo "#set four tabs to expand in vim" >> .vimrc
 echo "set tabstop=4" >> .vimrc
 echo "set expandtab" >> .vimrc
+
+# Get autoconf & automake
+sudo apt-get install autoconf
+sudo apt-get install automake
