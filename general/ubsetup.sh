b497523c272a801db91afab87ac3cfb37f2bc491
joshuabeard <joshuabeard92@gmail.com>
2018-02-28 21:06:51 -0500

    test ssh

--- general/ubsetup.sh
+++ general/ubsetup.sh
@@ -1,4 +1,4 @@
-## script for setting up useful preferences in ubuntu on my personal machine
+### script for setting up useful preferences in ubuntu on my personal machine
 
 # cs <dir>: combines cd <dir> and ls for simple navigation
 echo '# CS: combines cd and ls in one easy command' >> ~/.bashrc
