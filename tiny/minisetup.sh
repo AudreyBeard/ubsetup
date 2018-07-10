c2d640e54fbf67b75676d1bf1125d7641ce1c57c
Joshua Beard <josh.beard@kitware.com>
2018-07-10 16:58:46 -0400

    updates learned from work - mostly vim stuff

--- tiny/minisetup.sh
+++ tiny/minisetup.sh
@@ -1,4 +1,5 @@
 ### script for setting up useful preferences in ubuntu on my personal machine
+HERE=$PWD
 
 # cs <dir>: combines cd <dir> and ls for simple navigation
 echo '# CS: combines cd and ls in one easy command' >> ~/.bashrc
@@ -17,9 +18,6 @@ echo '# GITCLONE: makes git cloning easier and faster' >> ~/.bashrc
 echo 'function gitclone-ssh () {' >> ~/.bashrc
 echo '  git clone git@github.com:"$@".git' >> ~/.bashrc
 echo '}' >> ~/.bashrc
-
-echo "alias gitclone='gitclone-https'" >> ~/.bash_aliases
-. ~/.bashrc
 
 
 # Make a .local directory
@@ -41,17 +39,33 @@ sudo apt-get install rxvt-unicode -y
 
 # Install task
 sudo apt-get install taskwarrior -y
+echo '# Current Tasks' >> ~/.bashrc
+echo 'echo' >> ~/.bashrc
+echo 'echo "TASKS:"' >> ~/.bashrc
+echo 'task next' >> ~/.bashrc
 
 # Useful system stuff
 sudo apt-get install feh -y         # Background
 sudo apt-get install autocutsel -y  # Copy/Paste
 sudo apt-get install scrot -y       # screenshot
-sudo apt-get install compton -y     # compton
+
+
+# Vim
+# pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Vim Plugins
+cd ~/.vim/bundle
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+git clone https://github.com/vimwiki/vimwiki.git
+pip install flake8  # PEP8 linter
+cd $HERE
 
 
 # Font stuff
-cd ~/code; git clone https://github.com/Tecate/bitmap-fonts.git
-cd ubsetup/general
+cd ~/.local; git clone https://github.com/Tecate/bitmap-fonts.git
+cd $HERE
 mkdir -p ~/.bakfiles/etc/fonts/conf.d
 sudo mv /etc/fonts/conf.d/10* ~/.bakfiles/etc/fonts/conf.d
 sudo mv /etc/fonts/conf.d/70-no-bitmaps.conf ~/.bakfiles/etc/fonts/conf.d
@@ -63,23 +77,27 @@ sudo dpkg-reconfigure fontconfig
 
 # Copy files into etc folder
 mkdir -p ~/etc/
-cp ./etc/i3 ~/etc/i3
-cp ./etc/i3status ~/etc/i3status
-cp ./etc/Xresources ~/etc/Xresources
-cp ./etc/vimrc ~/.vimrc
-cp ./etc/taskrc ~/.taskrc
+cp $HERE/etc/* ~/etc/
+#cp ./etc/i3 ~/etc/i3
+#cp ./etc/i3status ~/etc/i3status
+#cp ./etc/Xresources ~/etc/Xresources
+#cp ./etc/vimrc ~/.vimrc
+#cp ./etc/taskrc ~/.taskrc
 
 # Make links (where apps expect them) to config files
-ln -s ~/etc/Xresources ~/.Xresources
 mkdir -p ~/.config/i3
-ln -s ~/etc/i3 ~/.config/i3/config
 mkdir -p ~/.config/i3status
+ln -s ~/etc/i3 ~/.config/i3/config
 ln -s ~/etc/i3status ~/.config/i3status/config
+ln -s ~/etc/Xresources ~/.Xresources
 ln -s ~/etc/vimrc ~/.vimrc
 ln -s ~/etc/taskrc ~/.taskrc
 
 # Make sure Nautilus doesn't launch gnome desktop to play nice with i3
-gsettings set org.gnome.desktop.background show-desktop-icons false
+if [ $(uname -v | grep Ubuntu -c) ]
+then
+    gsettings set org.gnome.desktop.background show-desktop-icons false
+fi
 
 # Use python3 as default in virtual environments
 export $VIRTUALENV_PYTHON=/usr/bin/python3
