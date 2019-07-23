13b916152bd38a25041c5b1b67804df34473c64a
Ubuntu <ubuntu@ip-172-31-38-1.ec2.internal>
2019-07-23 20:20:27 +0000

    remote server setup

---
+++ remote/install-system.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Random system stuff
+sudo apt-get install curl -y
+sudo apt-get install htop -y
+
+# Magic Wormhole for transfering files easily
+sudo apt-get install magic-wormhole -y
---
+++ remote/setup-vim.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+HERE=$HOME/code/ubsetup
+
+# Vim
+# pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Vim Plugins
+cd ~/.vim/bundle
+
+# PEP8 linter
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
+cp -r ~/.vim/bundle/syntastic/autoload ~/.vim/autoload/syntastic
+
+# Vimwiki!
+git clone https://github.com/vimwiki/vimwiki.git ~/.vim/bundle/vimwiki
+cp -r ~/.vim/bundle/vimwiki/autoload ~/.vim/autoload/vimwiki
+
+# NERDTree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+cp -r ~/.vim/bundle/nerdtree/autoload ~/.vim/autoload/nerdtree
+
+# Go back
+cd $HERE/setup
