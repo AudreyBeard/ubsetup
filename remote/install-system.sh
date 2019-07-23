dfa10d6fdb865fb298aab04a1eee7af33b8f5e0a
Ubuntu <ubuntu@ip-172-31-38-1.ec2.internal>
2019-07-23 20:31:08 +0000

    minor changes

--- remote/install-system.sh
+++ remote/install-system.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Random system stuff
-sudo apt-get install curl -y
-sudo apt-get install htop -y
+sudo apt-get install -y curl htop git
+git config --global push.default simple
 
 # Magic Wormhole for transfering files easily
 sudo apt-get install magic-wormhole -y
+
