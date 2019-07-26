9dbba9cd380a7a371baa86917fd21d337bc84f47
beard <joshuabeard92@gmail.com>
2019-07-25 22:15:52 -0400

    updates

--- general/setup/setup_python.sh
+++ general/setup/setup_python.sh
@@ -5,10 +5,12 @@ sudo apt-get install -y python3-pip
 sudo apt-get install -y python3-venv
 sudo apt-get install -y ipython3
 pip3 install virtualenv
+pip3 install virtualenvwrapper
 
-mkdir -p $HOME/.venv
-cd $HOME/.venv
-python3 -m venv Py3
+source $VIRTUALENVWRAPPER_SCRIPT
+#mkdir -p $HOME/.venv
+#cd $HOME/.venv
+#python3 -m venv Py3
 
 echo '# Default to using a venv' >> ~/.bashrc
 echo 'source $HOME/.venv/Py3/bin/activate' >> ~/.bashrc
